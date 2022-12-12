7404207f9c1017811a19f32a5e87aed8b7b5ca65
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-12 10:09:40 +0200

    first component

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,11 @@
+import "./app-info.css";
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Employee accounting in Company</h1>
+      <h2>Total employee:</h2>
+      <h2>Extra payment gets:</h2>
+    </div>
+  );
+};
+export default AppInfo;
