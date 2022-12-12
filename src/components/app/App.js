3457e7a7795bdf0a0d7e89c4f1929294ae469f56
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-12 10:17:59 +0200

    add search panel component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,17 @@
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+      </div>
+    </div>
+  );
 }
 
 export default App;
