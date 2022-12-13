654be3385b19731aa8094692ab2ed2abbd9645e1
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-13 10:14:33 +0200

    add form input change

--- src/components/employeers-add-form/employeers-add-form.js
+++ src/components/employeers-add-form/employeers-add-form.js
@@ -1,25 +1,49 @@
+import { Component } from "react";
 import "./employeers-add-form.css";
-const EmployeersAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add a new employeer</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="What is his/her name?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="Salary in $?"
-        />
-        <button type="submit" className="btn btn-outline-light">
-          Add
-        </button>
-      </form>
-    </div>
-  );
-};
+class EmployeersAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
+
+  onInputValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Add a new employeer</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            name="name"
+            value={name}
+            className="form-control new-post-label"
+            placeholder="What is his/her name?"
+            onChange={this.onInputValueChange}
+          />
+          <input
+            type="number"
+            name="salary"
+            value={salary}
+            className="form-control new-post-label"
+            placeholder="Salary in $?"
+            onChange={this.onInputValueChange}
+          />
+          <button type="submit" className="btn btn-outline-light">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeersAddForm;
